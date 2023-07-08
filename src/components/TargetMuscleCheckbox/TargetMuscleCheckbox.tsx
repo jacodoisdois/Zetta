1018c94d811f1260e8ab81f3696e0e20db1cde66
Jacó Magalhães <jacomagalhaes@gmail.com>
2023-07-08 01:06:13 -0300

    create muscle target component

--- src/components/TargetMuscleCheckbox/TargetMuscleCheckbox.tsx
+++ src/components/TargetMuscleCheckbox/TargetMuscleCheckbox.tsx
@@ -1,16 +1,78 @@
 import React from 'react';
-import Checkbox from 'expo-checkbox';
+import { TouchableOpacity, Text, StyleSheet, View } from 'react-native';
+import { muscles } from '../../types/Workout/WorkoutType';
 
+type MuscleOptionsProps = {
+  activeOptions: muscles[];
+  setActiveOptions: React.Dispatch<React.SetStateAction<muscles[]>>;
+};
 
-const TargetMuscleCheckbox: React.FC = () => {
+const MuscleOptions: React.FC<MuscleOptionsProps> = ({
+  activeOptions,
+  setActiveOptions,
+}) => {
+  const muscleOptions: muscles[] = [
+    'Shoulders',
+    'Chest',
+    'Back',
+    'Biceps',
+    'Triceps',
+    'Legs',
+    'Abs',
+    'Cardio',
+    'Other',
+  ];
+
+  const handlePress = (option: muscles) => {
+    if (activeOptions.includes(option)) {
+      setActiveOptions((prevState) => prevState.filter((o) => o !== option));
+    } else {
+      setActiveOptions((prevState) => [...prevState, option]);
+    }
+  };
 
   return (
-    <Checkbox
-    disabled={false}
-    value={targetMuscles.includes('Chest')}
-    onValueChange={() => handleTargetMuscles('Chest')}
-  />
+    <View style={styles.container}>
+      {muscleOptions.map((option) => (
+        <TouchableOpacity
+          key={option}
+          style={
+            activeOptions.includes(option)
+              ? styles.activeOption
+              : styles.inactiveOption
+          }
+          onPress={() => handlePress(option)}
+        >
+          <Text style={styles.optionText}>{option}</Text>
+        </TouchableOpacity>
+      ))}
+    </View>
   );
 };
 
-export default TargetMuscleCheckbox;
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+  inactiveOption: {
+    backgroundColor: 'lightgrey',
+    padding: 10,
+    margin: 5,
+    borderRadius: 5,
+  },
+  activeOption: {
+    backgroundColor: 'blue',
+    padding: 10,
+    margin: 5,
+    borderRadius: 5,
+  },
+  optionText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
+
+export default MuscleOptions;
--- src/screens/CreateWorkout.tsx
+++ src/screens/CreateWorkout.tsx
@@ -4,35 +4,45 @@ import { LinearGradient } from 'expo-linear-gradient';
 import InputExercise from '../components/InputExercise/InputExercise';
 import DefaultScreenButton from '../components/DefaultScreenButton/DefaultScreenButton';
 import { muscles, workoutType } from '../types/Workout/WorkoutType';
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import 'react-native-get-random-values';
 import { useNavigation } from '@react-navigation/native';
 import { addNewWorkout } from '../libs/SecureStore/Workout';
 import { ScrollView } from 'react-native-gesture-handler';
+import MuscleOptions from '../components/TargetMuscleCheckbox/TargetMuscleCheckbox';
 
 const CreateWorkout: React.FC = () => {
-  const [inputExercises, setInputExercises] = useState<{ [key: string]: string }>({});
+  const [inputExercises, setInputExercises] = useState<{
+    [key: string]: string;
+  }>({});
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [exercises, setExercises] = useState<string[]>([]);
-  const [targetMuscles, setTargetMuscles] = useState<muscles[]>([]);
+  const [activeMuscles, setActiveMuscles] = useState<muscles[]>([]);
   const [workoutName, setWorkoutName] = useState<string>('');
   const [workoutDescription, setWorkoutDescription] = useState<string>('');
   const navigator = useNavigation();
 
-
-  const handleInputExerciseChange = (inputName: string,exerciseName:string) => {
-    setInputExercises((prevInputValues) => ({ ...prevInputValues, [inputName]: exerciseName }));
+  const handleInputExerciseChange = (
+    inputName: string,
+    exerciseName: string
+  ) => {
+    setInputExercises((prevInputValues) => ({
+      ...prevInputValues,
+      [inputName]: exerciseName,
+    }));
   };
 
   const handleSaveWorkout = async () => {
-    const nonEmptyInputs = Object.values(inputExercises).filter((value) => value.trim() !== '');
+    const nonEmptyInputs = Object.values(inputExercises).filter(
+      (value) => value.trim() !== ''
+    );
     setExercises(nonEmptyInputs);
 
-    const workout : workoutType = {
+    const workout: workoutType = {
       id: uuidv4(),
       name: workoutName,
       description: workoutDescription,
-      targetMuscles: [],
+      targetMuscles: activeMuscles,
       exercises: nonEmptyInputs,
       createdAt: new Date(),
     };
@@ -41,54 +51,71 @@ const CreateWorkout: React.FC = () => {
     navigator.goBack();
   };
 
-  const handleTargetMuscles = async (muscle: muscles) => {
-    if (targetMuscles.includes(muscle)) {
-      await setTargetMuscles((prevTargetMuscles) => prevTargetMuscles.filter((m) => m !== muscle));
-    } else {
-      await setTargetMuscles((prevTargetMuscles) => [...prevTargetMuscles, muscle]);
-    }
-  };
-
-  const handleAddInput =  () => {
+  const handleAddInput = () => {
     const inputCount = Object.keys(inputExercises).length;
     const newInputName = `input${inputCount + 1}`;
-    setInputExercises((prevInputValues) => ({ ...prevInputValues, [newInputName]: '' }));
+    setInputExercises((prevInputValues) => ({
+      ...prevInputValues,
+      [newInputName]: '',
+    }));
   };
 
   return (
-    <LinearGradient colors={['#62c0ff', '#44a8eb', '#61bbf7']} style={styles.container}>
-      <View>
-        <Text style={styles.headerText}>Create a new Workout!</Text>
-      </View>
-      <View style={styles.contentContainer}>
-        <Text style={styles.contentHeaderText}>Workout Name:</Text>
-        <InputExercise placeholder='Set the workout name' onChangeText={setWorkoutName} />
+    <LinearGradient
+      colors={['#62c0ff', '#44a8eb', '#61bbf7']}
+      style={styles.container}
+    >
+      <ScrollView
+        contentInset={{ bottom: Object.keys(inputExercises).length * 20 }}
+      >
+        <View>
+          <Text style={styles.headerText}>Create a new Workout!</Text>
+        </View>
+        <View style={styles.contentContainer}>
+          <Text style={styles.contentHeaderText}>Workout Name:</Text>
+          <InputExercise
+            placeholder='Set the workout name'
+            onChangeText={setWorkoutName}
+          />
 
-        <Text style={styles.contentHeaderText}>Description:</Text>
-        <InputExercise onChangeText={setWorkoutDescription} />
+          <Text style={styles.contentHeaderText}>Description:</Text>
+          <InputExercise onChangeText={setWorkoutDescription} />
 
-        <Text style={styles.contentHeaderText}>Description:</Text>
+          <Text style={styles.contentHeaderText}>Target muscles:</Text>
 
-
+          <MuscleOptions
+            activeOptions={activeMuscles}
+            setActiveOptions={setActiveMuscles}
+          />
 
           <Text style={styles.contentHeaderText}>Exercises:</Text>
 
           <View style={styles.scrollViewContainer}>
-            <ScrollView  contentInset={{bottom: Object.keys(inputExercises).length * 20}}>
             {Object.entries(inputExercises).map(([name]) => (
               <InputExercise
                 key={name}
                 placeholder={`Exercise ${name.substring(5)}`}
-                onChangeText={(text) => { if(text && text !== '') handleInputExerciseChange(name, text)}}
+                onChangeText={(text) => {
+                  if (text && text !== '')
+                    handleInputExerciseChange(name, text);
+                }}
               />
             ))}
             <View style={styles.buttonContainer}>
-            <DefaultScreenButton buttonName="Create" onPress={handleSaveWorkout} color='#1E90FF' />
-            <DefaultScreenButton buttonName="Add Input" onPress={handleAddInput} color='#1E90FF' />
+              <DefaultScreenButton
+                buttonName='Create'
+                onPress={handleSaveWorkout}
+                color='#1E90FF'
+              />
+              <DefaultScreenButton
+                buttonName='Add Input'
+                onPress={handleAddInput}
+                color='#1E90FF'
+              />
             </View>
-            </ScrollView>
           </View>
         </View>
+      </ScrollView>
     </LinearGradient>
   );
 };
@@ -116,8 +143,8 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-  },scrollViewContainer: {
   },
+  scrollViewContainer: {},
   scrollViewContent: {
     height: 'min-content',
   },
